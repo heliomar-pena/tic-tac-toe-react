08cd125d25400614c8e5853abb0b56183f1c974b
Victor Peña <katsukeinz@gmail.com>
2022-03-13 18:32:43 -0400

    Fixed bug

--- src/components/Moves.js
+++ src/components/Moves.js
@@ -4,7 +4,7 @@ const Moves = ({history, selected, onClick}) => history.map((value, index) => {
         `${move.currentPlayer} play on column ${move.col}, row ${move.row}` :
         `Go to game start`;
 
-    const className = selected === index ? 'border' : undefined;
+    const className = selected === index ? 'bolder' : undefined;
 
     return (
         <li key={index} className={className}>
